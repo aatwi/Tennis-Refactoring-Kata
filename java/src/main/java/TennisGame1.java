845103417268590473e69fddc3949db0850d00ab
aatwi <@twi20185>
2020-04-23 15:12:51 +0300

    First Refactoring Attempt

--- java/src/main/java/TennisGame1.java
+++ java/src/main/java/TennisGame1.java
@@ -1,6 +1,35 @@
+enum Score {
+    LOVE(0, "Love"),
+    FIFTEEN(1, "Fifteen"),
+    THIRTY(2, "Thirty"),
+    FORTY(3, "Forty");
+
+    private final int intScore;
+    private final String stringScore;
+
+    Score(int intScore, String stringScore) {
+        this.intScore = intScore;
+        this.stringScore = stringScore;
+    }
+
+    public static String getStringScoreOf(int score) {
+        for (Score value : values()) {
+            if (value.intScore == score) {
+                return value.stringScore;
+            }
+        }
+        throw new UnsupportedOperationException();
+    }
+
+    public static String getStringTieScoreOf(int score) {
+        if(score < 3) {
+            return getStringScoreOf(score) + "-All";
+        }
+        return "Deuce";
+    }
+}
 
 public class TennisGame1 implements TennisGame {
-    
     private int m_score1 = 0;
     private int m_score2 = 0;
     private String player1Name;
@@ -12,65 +41,40 @@ public class TennisGame1 implements TennisGame {
     }
 
     public void wonPoint(String playerName) {
-        if (playerName == "player1")
+        if (playerName.equals(player1Name))
             m_score1 += 1;
         else
             m_score2 += 1;
     }
 
     public String getScore() {
-        String score = "";
-        int tempScore=0;
-        if (m_score1==m_score2)
-        {
-            switch (m_score1)
-            {
-                case 0:
-                        score = "Love-All";
-                    break;
-                case 1:
-                        score = "Fifteen-All";
-                    break;
-                case 2:
-                        score = "Thirty-All";
-                    break;
-                default:
-                        score = "Deuce";
-                    break;
-                
-            }
+        if (m_score1 == m_score2) {
+            return getScoreOnTie();
+        } else if (m_score1 >= 4 || m_score2 >= 4) {
+            return getScoreGreaterThan4Points();
+        } else {
+            return getScoreLessThan4Points();
         }
-        else if (m_score1>=4 || m_score2>=4)
-        {
-            int minusResult = m_score1-m_score2;
-            if (minusResult==1) score ="Advantage player1";
-            else if (minusResult ==-1) score ="Advantage player2";
-            else if (minusResult>=2) score = "Win for player1";
-            else score ="Win for player2";
+    }
+
+    private String getScoreOnTie() {
+        return Score.getStringTieScoreOf(m_score1);
+    }
+
+    private String getScoreGreaterThan4Points() {
+        int diffScore = m_score1 - m_score2;
+        if (diffScore == 1 || diffScore == -1) {
+            return "Advantage " + getWinner(m_score1, m_score2);
+        } else {
+            return "Win for " + getWinner(m_score1, m_score2);
         }
-        else
-        {
-            for (int i=1; i<3; i++)
-            {
-                if (i==1) tempScore = m_score1;
-                else { score+="-"; tempScore = m_score2;}
-                switch(tempScore)
-                {
-                    case 0:
-                        score+="Love";
-                        break;
-                    case 1:
-                        score+="Fifteen";
-                        break;
-                    case 2:
-                        score+="Thirty";
-                        break;
-                    case 3:
-                        score+="Forty";
-                        break;
-                }
-            }
-        }
-        return score;
+    }
+
+    private String getWinner(int player1Score, int player2Score) {
+        return player1Score > player2Score ? player1Name : player2Name;
+    }
+
+    private String getScoreLessThan4Points() {
+        return Score.getStringScoreOf(m_score1) + "-" + Score.getStringScoreOf(m_score2);
     }
 }
